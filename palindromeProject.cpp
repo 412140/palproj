fc3b808701b6703a8d5b4cbe031e265451ad89fd
412140 <144371963+412140@users.noreply.github.com>
2023-10-01 19:28:09 -0700

    Update palindromeProject.cpp
    it works now

--- palindromeProject.cpp
+++ palindromeProject.cpp
@@ -19,29 +19,73 @@ https://cplusplus.com/reference/cctype/
 
 int main()
 {
-bool isPalindrome;
-char str[80];
-char str2[80];
+File Edit Options Buffers Tools C++ Help
+#include <iostream>
+#include <cstring>
+#include <cctype>
 
-cout << "input your string, max of 80 characters. \n";
-cin.get(str, 80);
- int length = strlen(str); //get length
- int x = 0; //initialize variable that im gonna use to target parts of str2
- for(int i = length - 1; i >= 0; i--)
-   {
-     if(!ispunct(str[i]) && !isspace(str[i])) //from https://cplusplus.com/reference/cctype/ispunct/ and https://cplusplus.com/reference/cctype/isspace/
-       {
-       str2[x] = tolower(str[i]); //loop is filling str2 backwards with a lowercase of whatever str[i] is only if it meets the condition that str[i] is NOT a space or a punctuation mark
-       }
-     x++;
-   }
- if(strcmp(str, str2) == 0) //function to compare cstrings and returns a value on what the result was
-   {
-     isPalindrome = true;
-     cout << "Palindrome";
-   } else {
-   cout << "Not a palindrome";
- }
+using namespace std;
+
+/*-
+ghazi abbas cpp shs sem1 galbraith
+program should be able to determine whether or not an inputed string is a palindrome
+this is accomplished by comparing arrays with cstring functions
+
+Sources:
+https://cplusplus.com/doc/tutorial/ntcs/
+https://cplusplus.com/reference/cstring/
+https://cplusplus.com/reference/cctype/
+*/
+
+
+
+int main()
+{
+
+    char str[80];
+    char str2[80];
+    char str3[80];
+
+    cout << "input your string, max of 80 characters. \n";
+    cin.get(str, 80);
+    int length = strlen(str);
+
+    // Fill str3 with alphanumeric characters from str (forward)
+    int x = 0;
+    for (int j = 0; j < length; j++) {
+        if (isalnum(str[j])) {
+            str3[x++] = tolower(str[j]);
+        }
+    }
+    str3[x] = '\0';
+
+    // Fill str2 with alphanumeric characters from str (backward)
+    int y = 0;
+    for (int i = length - 1; i >= 0; i--) {
+        if (isalnum(str[i])) {
+            str2[y] = tolower(str[i]);
+            y++;
+        }
+    }
+    str2[y] = '\0';
+
+    cout << "Original cstring: " << str << endl;
+    cout << "c string forward: " << str3 << endl;
+    cout << " cstring backward: " << str2 << endl;
+
+    // Check if str3 and str2 are the sam
+    if (strcmp(str3, str2) == 0) {
+        cout << "palindrome.";
+    } else {
+        cout << "not a palindrome.";
+    }
+}
+
+
+
+
+
+
 
 }
 
